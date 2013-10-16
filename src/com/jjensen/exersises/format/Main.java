9d8030fcec20669b4947c157427a7e44dabaf5e9
jjensen <haiyun211@gmail.com>
2013-10-16 15:38:32 -0600

    Fixed infinite loop

--- src/com/jjensen/exersises/format/Main.java
+++ src/com/jjensen/exersises/format/Main.java
@@ -3,6 +3,14 @@ package com.jjensen.exersises.format;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Order order = new Order();
+        order.addItem(new Product("Monitor", 2, 150.00));
+        order.addItem(new Product("1TB HD", 2, 150.00));
+        order.addItem(new Product("Tower", 1, 300.00));
+
+        Invoice invoice = order.generateInvoice();
+        invoice.print();
+
+
     }
 }
