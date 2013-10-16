971654c570820975d538dba74ababf3f672b9ee5
jjensen <haiyun211@gmail.com>
2013-10-16 16:30:16 -0600

    Fixed iteration issue

--- src/com/jjensen/exersises/format/Main.java
+++ src/com/jjensen/exersises/format/Main.java
@@ -7,6 +7,7 @@ public class Main {
         order.addItem(new Product("Monitor", 2, 150.00));
         order.addItem(new Product("1TB HD", 2, 150.00));
         order.addItem(new Product("Tower", 1, 300.00));
+        order.addItem(new Product("SomthingReally Long", 1, 12.00));
 
         Invoice invoice = order.generateInvoice();
         invoice.print();
